1368f1958473f2214023694d83039f6155316694
Vladimir Oberemok <nihtalak@gmail.com>
2013-05-16 04:32:22 -0700

    convert tabs to spaces

--- library.rb
+++ library.rb
@@ -3,66 +3,66 @@ require 'set'
 # TASK 1
 
 class BookOrder
-	attr_reader :book_name, :name, :order_date, :issue_date
+  attr_reader :book_name, :name, :order_date, :issue_date
 
-	def initialize(book_name, name, issue_date = nil, order_date = Time.now)
-		@book_name, @name, @order_date, @issue_date = book_name, name, order_date, issue_date	
-	end
+  def initialize(book_name, name, issue_date = nil, order_date = Time.now)
+    @book_name, @name, @order_date, @issue_date = book_name, name, order_date, issue_date 
+  end
 
-	def delay
-		@issue_date ? @issue_date - @order_date	: nil 
-	end
+  def delay
+    @issue_date ? @issue_date - @order_date : nil 
+  end
 end
 
 class Library
-	def initialize
-		@orders = []
-	end
+  def initialize
+    @orders = []
+  end
 
   # the smallest period for which library found a book
-	def min_delay
-		if @orders.empty?
-			return "No orders"
-		elsif @orders.size == 1
-			delay =  @orders.first.delay
-		else
-			delay = @orders.min { |a, b| a.delay <=> b.delay }.delay
-		end
+  def min_delay
+    if @orders.empty?
+      return "No orders"
+    elsif @orders.size == 1
+      delay =  @orders.first.delay
+    else
+      delay = @orders.min { |a, b| a.delay <=> b.delay }.delay
+    end
 
-		Time.at(delay).getgm.strftime("%H:%M:%S")
-	end
+    Time.at(delay).getgm.strftime("%H:%M:%S")
+  end
 
-	# how many orders were not satisfied 
-	def not_satisfied
-		@orders.inject(0) { |s, order| order.issue_date.nil? ? s + 1 : s }
-	end
+  # how many orders were not satisfied 
+  def not_satisfied
+    @orders.inject(0) { |s, order| order.issue_date.nil? ? s + 1 : s }
+  end
 
-	# who often takes the book
-	def best_reader(book)
-		readers = Hash.new(0)
-		@orders.each { |order| readers[order.name] += 1 if order.book_name == book }	
-		readers.max_by{ |key, value| value }.first
-	end
+  # who often takes the book
+  def best_reader(book)
+    readers = Hash.new(0)
+    @orders.each { |order| readers[order.name] += 1 if order.book_name == book }  
+    readers.max_by{ |key, value| value }.first
+  end
 
-	# what is the most popular book
-	def popular_book
-		books = Hash.new(0)
-		@orders.each { |order| books[order.book_name] += 1 }	
-		book = books.max_by{ |key, value| value }.first
-	end
+  # what is the most popular book
+  def popular_book
+    books = Hash.new(0)
+    @orders.each { |order| books[order.book_name] += 1 }  
+    book = books.max_by{ |key, value| value }.first
+  end
 
-	# how many people ordered one of the __number__ most popular books.
-	def peoples_count_ordered_one_popular_book(num)
-		books = Hash.new(0)
-		@orders.each { |order| books[order.book_name] += 1 }
-		books = books.sort { |a, b| b.last <=> a.last }.take(num).map &:first
-		# puts books
-		readers = Set.new
-		@orders.each { |order| readers << order.name if books.include? order.book_name }
-		readers.size
-	end
+  # how many people ordered one of the __number__ most popular books.
+  def peoples_count_ordered_one_popular_book(num)
+    books = Hash.new(0)
+    @orders.each { |order| books[order.book_name] += 1 }
+    books = books.sort { |a, b| b.last <=> a.last }.take(num).map &:first
+    # puts books
+    readers = Set.new
+    @orders.each { |order| readers << order.name if books.include? order.book_name }
+    readers.size
+  end
 
-	def add(book_record)
-		@orders << book_record
-	end
+  def add(book_record)
+    @orders << book_record
+  end
 end
--- library_spec.rb
+++ library_spec.rb
@@ -1,69 +1,69 @@
 require "./library.rb"
 
 describe Library do
-	BOOKS = ["War and Peace", "The Brothers Karamazov", "The Captain's Daughter", "Crime and Punishment"]
-	READERS = ["John", "Joe", "Jack"]
+  BOOKS = ["War and Peace", "The Brothers Karamazov", "The Captain's Daughter", "Crime and Punishment"]
+  READERS = ["John", "Joe", "Jack"]
 
-	before(:each) do
-		@lib = Library.new
-	end
+  before(:each) do
+    @lib = Library.new
+  end
 
-	it "returns 'No orders' for getting min delay on no orders library"	do
-		@lib.min_delay.should eq("No orders")
-	end
+  it "returns 'No orders' for getting min delay on no orders library" do
+    @lib.min_delay.should eq("No orders")
+  end
 
-	it "returns 00:01:40 for getting min delay on one order library" do
-		order = Time.new
-		issue = order + 100
-		@lib.add(BookOrder.new(BOOKS.first, READERS.first, issue, order))
-		@lib.min_delay.should eq("00:01:40")
-	end
+  it "returns 00:01:40 for getting min delay on one order library" do
+    order = Time.new
+    issue = order + 100
+    @lib.add(BookOrder.new(BOOKS.first, READERS.first, issue, order))
+    @lib.min_delay.should eq("00:01:40")
+  end
 
-	it "returns 00:01:00 for getting min delay on few order library" do
-		order = Time.new
-		issue = order + 100
-		@lib.add(BookOrder.new(BOOKS.first, READERS.first, issue, order))
+  it "returns 00:01:00 for getting min delay on few order library" do
+    order = Time.new
+    issue = order + 100
+    @lib.add(BookOrder.new(BOOKS.first, READERS.first, issue, order))
 
-		order = Time.new
-		issue = order + 60
-		@lib.add(BookOrder.new(BOOKS.last, READERS.last, issue, order))
+    order = Time.new
+    issue = order + 60
+    @lib.add(BookOrder.new(BOOKS.last, READERS.last, issue, order))
 
-		@lib.min_delay.should eq("00:01:00")
-	end
+    @lib.min_delay.should eq("00:01:00")
+  end
 
-	it "returns 2 for getting not satisfied orders" do
-		5.times { @lib.add(BookOrder.new(BOOKS.first, READERS.last, Time.now)) }
-		2.times { @lib.add(BookOrder.new(BOOKS.first, READERS.last)) }
-	end
+  it "returns 2 for getting not satisfied orders" do
+    5.times { @lib.add(BookOrder.new(BOOKS.first, READERS.last, Time.now)) }
+    2.times { @lib.add(BookOrder.new(BOOKS.first, READERS.last)) }
+  end
 
-	it "returns #{READERS.first} for getting who often takes the #{BOOKS.first} book" do
-		3.times { @lib.add(BookOrder.new(BOOKS.first, READERS.first)) }
-		2.times { @lib.add(BookOrder.new(BOOKS.first, READERS.last)) }
+  it "returns #{READERS.first} for getting who often takes the #{BOOKS.first} book" do
+    3.times { @lib.add(BookOrder.new(BOOKS.first, READERS.first)) }
+    2.times { @lib.add(BookOrder.new(BOOKS.first, READERS.last)) }
 
-		@lib.best_reader(BOOKS.first).should eq(READERS.first)
-	end		
+    @lib.best_reader(BOOKS.first).should eq(READERS.first)
+  end   
 
-	it "returns #{READERS.last} for getting who often takes the #{BOOKS.last} book" do
-		3.times { @lib.add(BookOrder.new(BOOKS.last, READERS.first)) }
-		5.times { @lib.add(BookOrder.new(BOOKS.last, READERS.last)) }
+  it "returns #{READERS.last} for getting who often takes the #{BOOKS.last} book" do
+    3.times { @lib.add(BookOrder.new(BOOKS.last, READERS.first)) }
+    5.times { @lib.add(BookOrder.new(BOOKS.last, READERS.last)) }
 
-		@lib.best_reader(BOOKS.last).should eq(READERS.last)
-	end
+    @lib.best_reader(BOOKS.last).should eq(READERS.last)
+  end
 
-	it "returns #{BOOKS.last} for getting most popular book" do
-		3.times { @lib.add(BookOrder.new(BOOKS.first, READERS.first)) }
-		5.times { @lib.add(BookOrder.new(BOOKS.last, READERS.last)) }
+  it "returns #{BOOKS.last} for getting most popular book" do
+    3.times { @lib.add(BookOrder.new(BOOKS.first, READERS.first)) }
+    5.times { @lib.add(BookOrder.new(BOOKS.last, READERS.last)) }
 
-		@lib.popular_book.should eq(BOOKS.last)
-	end
+    @lib.popular_book.should eq(BOOKS.last)
+  end
 
-	it "returns 2 for getting how many people ordered one of the three most popular books." do
-		5.times { @lib.add(BookOrder.new(BOOKS.last, READERS.last)) }
-		4.times { @lib.add(BookOrder.new(BOOKS[2], READERS.first)) }
-		4.times { @lib.add(BookOrder.new(BOOKS.first, READERS.first)) }
+  it "returns 2 for getting how many people ordered one of the three most popular books." do
+    5.times { @lib.add(BookOrder.new(BOOKS.last, READERS.last)) }
+    4.times { @lib.add(BookOrder.new(BOOKS[2], READERS.first)) }
+    4.times { @lib.add(BookOrder.new(BOOKS.first, READERS.first)) }
 
-		1.times { @lib.add(BookOrder.new(BOOKS[1], READERS[1])) }
-		2.times { @lib.add(BookOrder.new(BOOKS[1], READERS.first)) }
-		@lib.peoples_count_ordered_one_popular_book(3).should eq(2)
-	end
+    1.times { @lib.add(BookOrder.new(BOOKS[1], READERS[1])) }
+    2.times { @lib.add(BookOrder.new(BOOKS[1], READERS.first)) }
+    @lib.peoples_count_ordered_one_popular_book(3).should eq(2)
+  end
 end
